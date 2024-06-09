c3cfeb3cb5de96f10f494c161251cafc2c014cf6
subhankar <subhankar999sj@gmail.com>
2024-06-09 12:25:38 +0530

    reg-msg update

--- src/Components/RegisteredSuccessfullyMsg/RegisteredSuccessfully.js
+++ src/Components/RegisteredSuccessfullyMsg/RegisteredSuccessfully.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './RegisteredSuccessfully.css'
-import greenTick from '../../Assets/Images/green-tick.png'
+import greenTick from '../../Assets/Images/green-tick.jpg'
 import { useNavigate } from 'react-router-dom'
 
 export default function RegisteredSuccessfully() {
@@ -13,7 +13,7 @@ export default function RegisteredSuccessfully() {
     <div className='registration-body-div'>
       <div className='registration-msg-div'>
         <img src={greenTick} className='green-tick'/>
-        <h3>Account Created Successflly</h3>
+        <h3>Account Created Successfully</h3>
         <button onClick={home} className="login-here-btn">Home</button>
       </div>
     </div>
